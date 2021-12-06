2df79568b6fb6604bd9ba493cbc4db3456611656
marianmitu <marianp.mitu@ulbsibiu.ro>
2021-12-06 10:25:22 +0200

    Added authentication and roles

--- src/main/java/com/park/parkinglot2/servlet/Logout.java
+++ src/main/java/com/park/parkinglot2/servlet/Logout.java
@@ -4,18 +4,9 @@
  */
 package com.park.parkinglot2.servlet;
 
-import com.park.parkinglot2.common.CarDetails;
-import com.park.parkinglot2.ejb.CarBean;
-import com.park.parkinglot2.entity.Car;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
-import javax.annotation.security.DeclareRoles;
-import javax.inject.Inject;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.HttpConstraint;
-import javax.servlet.annotation.ServletSecurity;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,19 +16,9 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author ASUS
  */
+@WebServlet(name = "Logout", urlPatterns = {"/Logout"})
+public class Logout extends HttpServlet {
 
-@DeclareRoles({"AdminRole","ClientRole"})
-@ServletSecurity(
-        value= @HttpConstraint(
-                rolesAllowed={"AdminRole"}
-        )
-)
-
-@WebServlet(name = "Cars", urlPatterns = {"/Cars"})
-public class Cars extends HttpServlet {
-
-    @Inject
-    private CarBean carBean;
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -55,10 +36,10 @@ public class Cars extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet Cars</title>");            
+            out.println("<title>Servlet Logout</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet Cars at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet Logout at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
@@ -76,12 +57,8 @@ public class Cars extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.setAttribute("activePage", "Cars");
-        request.setAttribute("numberOfFreeParkingSpots", 10);
-        
-        List <Car> cars = carBean.getAllCars();
-        request.setAttribute("cars", cars);
-        request.getRequestDispatcher("/WEB-INF/pages/cars.jsp").forward(request,response);
+        request.logout();
+        response.sendRedirect(request.getContextPath());
     }
 
     /**
@@ -95,15 +72,7 @@ public class Cars extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String[] carIdsAsString = request.getParameterValues("car_ids");
-        if(carIdsAsString!=null){
-            List <Integer> carIds = new ArrayList<>();
-            for(String carIdAsString : carIdsAsString){
-                carIds.add(Integer.parseInt(carIdAsString));
-            }
-            carBean.deleteCarsByIds(carIds);
-        }
-        response.sendRedirect(request.getContextPath() + "/Cars");
+        processRequest(request, response);
     }
 
     /**
